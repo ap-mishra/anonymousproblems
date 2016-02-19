886c0dc0c21ccaa63428e05d6e701ed1287e8520
wrecker-mishra <er.ashwin.mishra@gmail.com>
2016-02-18 22:13:06 -0500

    Code for 9.py

--- src/9.py
+++ src/9.py
@@ -16,16 +16,32 @@ Find the product abc.
 if __name__=='__main__':
     #do something
     s = []
-    n=3
+    n=1000
     for a in range(1,n+1):
         for b in range(1,n+1):
             if (n-a-b>=1):
                 ar = sorted([a,b,n-a-b])
                 print ar
-                print "elements : "+str(ar[0] + str(ar[1]) + str(ar[2])
+                print "elements : "+str(ar[0]) + str(ar[1]) + str(ar[2])
                 s.append(ar)
+    print sorted(s)
 
-    print s
+    for tuple in s:
+        print "Tuple = : " + str(tuple)
+        max1 = tuple[2]
+        mid1 = tuple[1]
+        min1 = tuple[0]
+        print "Components : max : "+ str(max1) + ", mid = " + str(mid1) + ", min1 = " + str(min1)
+        if (np.power(max1,2) == np.power(min1,2) + np.power(mid1,2)):
+            print "Success ...."
+            print tuple
+            exit(0)
+
+
+
+
+
+
 
 #    for a in x:
 #        b = a + 1
